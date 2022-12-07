4a83fd2143e277835739b019c4bf305822982126
Belen Carrion <belle.193@hotmail.com>
2022-12-07 21:44:20 +0100

    Corregir operadores

--- 2 operadores/operadores.js
+++ 2 operadores/operadores.js
@@ -1,17 +1,9 @@
 let a = true;
 let b = false;
- 
-let c = a & b; // devuelve true si TODO es true -> false
-c = a | b; // devuelve true si AL MENOS UNO es true -> true
-c = a ^ b; // devuelve true SOLO SI UNO es true -> true
 
-true | true; // true
-true ^ true; // false
-
-c = a && b; // devuelve true si TODO es true -> false
-
-false && true; // con && dejo de mirar cuando encuentro false porque se que va a ser false
-false & true; // va a evaluar la expresion entera
-
-true || false; // con || dejo de mirar cuando encuentro true porque se que va a ser true
-true | false; // mira la expresión entera
+let c = a && b; // devuelve true si TODO es true -> false
+console.log(c);
+c = a || b; // devuelve true si AL MENOS UNO es true -> true
+console.log(c);
+c = !a; // lo contrario de a. lo contrario de true -> false
+console.log(c);
